f417c09398f166a389854ed7299052091340552d
Litrer <106829230+L1itrer@users.noreply.github.com>
2026-04-11 22:56:00 +0200

    Add disassembly option to build.sh

--- build.sh
+++ build.sh
@@ -12,8 +12,15 @@ for arg in "$@"; do declare $arg='1'; done
 if [ -v clang ]; then CC="clang"; fi
 
 CFLAGS="-Wall -Wextra -nostdlib -fno-builtin -fno-unwind-tables -fno-stack-protector -static"
+
+# if [ -v asm ]; then CFLAGS+=" -c -S"; fi
 DEBUG_FLAGS="-g -DCLOSER_DEBUG=1 -DCLOSER_DISABLE=1"
 RELEASE_FLAGS="-O2"
 
-if [ -v release ]; then $CC $RELEASE_FLAGS $CFLAGS ./src/main.c -o ./build/closer; fi
-if [ ! -v release ]; then $CC $DEBUG_FLAGS $CFLAGS ./src/main.c -o ./build/closer; fi
+SPECIFIC_FLAGS=""
+if [ -v release ]; then SPECIFIC_FLAGS=$RELEASE_FLAGS; fi
+if [ ! -v release ]; then SPECIFIC_FLAGS=$DEBUG_FLAGS; fi
+
+$CC $SPECIFIC_FLAGS $CFLAGS ./src/main.c -o ./build/closer
+if [ -v asm ]; then $CC $SPECIFIC_FLAGS $CFLAGS ./src/main.c -o ./build/closer.asm -c -S; fi
+# if [ ! -v release ]; then $CC $DEBUG_FLAGS $CFLAGS ./src/main.c -o ./build/closer; fi
